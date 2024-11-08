2708e8b6861b34f3a4dc3e87fc5bd96a3045f88f
unknown <emmco96@gmail.com>
2024-11-08 14:20:46 +0100

    new

--- script.js
+++ script.js
@@ -140,3 +140,21 @@ function toggleFAQ(id) {
   var div = document.getElementById('div' + id);
   div.classList.toggle('show');
 }
+
+  // Show the button when the user scrolls down 100px
+  window.onscroll = function() {
+    const button = document.getElementById("scrollToTopBtn");
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      button.style.display = "block";
+    } else {
+      button.style.display = "none";
+    }
+  };
+
+  // Scroll to top function
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  }
